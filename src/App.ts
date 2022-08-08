74c85af7da3059c88caebc9d01123f6403cab0c7
Vladislav Zubakin <shaqimusic@gmail.com>
2022-08-08 16:49:13 +0300

    feat: add footer

--- src/App.ts
+++ src/App.ts
@@ -14,6 +14,18 @@ const App = async (): Promise<AppObj> => {
   const container = render<HTMLDivElement>('div', null, 'body');
   const header = render<HTMLElement>('header', null, container);
   const main = render<HTMLElement>('main', null, container);
+  const footer = render<HTMLElement>('footer', null, container);
+  const ghLink = render<HTMLAnchorElement>('a', 'footer-link', footer);
+  const year = render<HTMLSpanElement>('span', 'footer-info', footer);
+  const rsLink = render<HTMLAnchorElement>('a', 'footer-link', footer);
+  const ghLogo = render<HTMLImageElement>('img', 'gh-logo', ghLink);
+  const rsLogo = render<HTMLImageElement>('img', 'rs-logo', rsLink);
+
+  ghLink.href = 'https://github.com/shaqi-dev';
+  rsLink.href = 'https://rs.school/js/';
+  year.innerText = '2022';
+  ghLogo.src = './assets/svg/gh-logo.svg';
+  rsLogo.src = './assets/svg/rsschool-logo.svg';
 
   container.id = 'app';
 
